2e3bb95f69f899e2840ed38d9df07087fa439b76
m305-12\w401 <j841013@yahoo.com.tw>
2018-03-22 18:10:56 +0800

    亂數產生器

--- Program.cs
+++ Program.cs
@@ -13,8 +13,11 @@ namespace lottery
             // 建立亂數物件
             Random rand = new Random();
 
+            int count = 0;
+            
             // 重複產生
-            while(true){
+            while( count < 10 )
+                {            
                 // 用亂數物件產生亂數
                 int r1 = rand.Next(1, 51);
                 int r2 = rand.Next(1, 51);
@@ -24,8 +27,11 @@ namespace lottery
                 int r6 = rand.Next(1, 51);
 
                 // 顯示
-                Console.WriteLine("號碼 : {0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", r1, r2, r3, r4, r5, r6);
+                Console.WriteLine("號碼 : [ {0:00} ] {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", count, r1, r2, r3, r4, r5, r6);
+
+                count++;
             }
+            
             // 避免程式碼結束
             Console.ReadLine();
         }
